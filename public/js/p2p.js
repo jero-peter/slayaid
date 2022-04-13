550089e5dcf4587cf53b27dd8de6ac2732eea210
sudo-g1itch <joeltommy98@gmail.com>
2022-04-13 13:53:54 +0530

    cursor

--- public/js/p2p.js
+++ public/js/p2p.js
@@ -92,3 +92,31 @@ async function launch() {
         });
     });
 }
+
+
+function prepareAgentActionObjects() {
+
+    var createCursorDiv = document.createElement("div");
+    createCursorDiv.classList.add("text-dark");
+    createCursorDiv.name = "cursorDiv";
+    createCursorDiv.id = "cursorDiv";
+    createCursorDiv.style.position = 'absolute';
+    createCursorDiv.style.top = '0';
+    createCursorDiv.style.left = '0';
+    createCursorDiv.style.zIndex = '9999';
+    document.body.appendChild(createCursorDiv);
+
+    var createCursorImg = document.createElement("img");
+    createCursorImg.id = "cursorImg";
+    createCursorImg.setAttribute('src', 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABzUlEQVR4XpWTT2iSYRzHP4Y6t7JLw4NCXRYrZl2adJ+jUVTEDuHuQUHsFgRBEEGNLalwM3UrijG0Fv1Z9Mc61CUiWKbnbeYke41OQk1998731+tlaLje7QNfntP3w4/f8zyIyHoeTM9omNPU2UYDtVrNOj0Tl6l79z1sjmaBpmmcPHEcp9NZiMTuBrYuWFujo6OdY0cH6OzclbgTnUxsSaDXdKqVCsvLeQaO+Nnb1RUYD0cKmxeIoKqr/P6zwsJiFq+3B19vr+d2aELCkZjHVIDoqKsqbXYbiFAsFnG5XPT3+wEK4+Fo4P8T6IZArQvs2I3YbDaq1TLtDgf+vr76chOhicizDQUiGAKNNqNQqZRxGOfbd+958vQ5r14nKZVKAKdowPrvDupTKMovvmYynB4cxNuznzfJZOxmcGwKAGD4/LmNdiCslMuk0hm9+EOZzeXz7Ovuxu12DwGphrQWWCwwP/+Fz58+nr0xNnIhnc7g3LGdgwe8O0dGg0OmtyBiQVGUucezjx4C37PfspMLi0sc9vnqzzxu+jGAQ8Ae1sEdCkdl7sVLuXT5yodWHSvNpGhGyeVy8Z+Ksvv6tatnaIEVE24FRy8CDiBLC/4C3kjgJkXCw9gAAAAASUVORK5CYII=');
+    document.getElementById("cursorDiv").appendChild(createCursorImg);
+
+    var createCursorSpan = document.createElement("div");
+    createCursorSpan.classList.add("text-dark");
+    createCursorSpan.id = "cursorText";
+    createCursorSpan.innerHTML = "Agent";
+    document.getElementById("cursorImg").appendChild(createCursorSpan);
+
+    // $('body').append('<div id="cursorDiv" class="text-dark"  style="position:absolute;top:0;left:0;z-index:9999;"><img src=""><span id="cursorText">Agent</span></div>');
+}
+
